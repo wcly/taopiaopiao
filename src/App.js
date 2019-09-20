ec83a61867557806a9582fb333e2c88c948312dd
wcly <git@github.com:wcly>
2019-09-20 22:22:48 +0800

    加入用户页面

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './routes/Home';
+import User from './routes/User';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={Home} exact />
+        <Route path="/user" component={User} exact />
       </Switch>
     </BrowserRouter>
   );
